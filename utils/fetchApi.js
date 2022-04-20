4e9c38fbfcf07db3d225ef7d7d40df8e8f422e85
thisiskas <dimowookasime@gmail.com>
2022-04-21 00:25:52 +0200

    modify

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -5,8 +5,8 @@ export const baseUrl = "https://bayut.p.rapidapi.com";
 export const fetchApi = async (url) => {
   const { data } = await axios.get(url, {
     headers: {
-      "X-RapidAPI-Host": "bayut.p.rapidapi.com",
-      "X-RapidAPI-Key": process.env.NEXT_PUBLIC_RAPID_API_KEY,
+      X_RapidAPI_Host: "bayut.p.rapidapi.com",
+      X_RapidAPI_Key: process.env.NEXT_PUBLIC_RAPID_API_KEY,
     },
   });
 
